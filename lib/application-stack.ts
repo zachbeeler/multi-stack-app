cc41263a56ca122261a1f486ec532be985405782
zachbeeler <zach@scalesec.com>
2023-12-08 14:15:45 -0600

    feature: first lambda commit

--- lib/application-stack.ts
+++ lib/application-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class ApplicationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MultiStackAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // define the Lambda function
+    const hello = new lambda.Function(this, 'HelloHandler', {
+        runtime: lambda.Runtime.NODEJS_16_X,
+        code: lambda.Code.fromAsset('lambda'),
+        handler: 'hello.handler'
+    });
   }
 }
