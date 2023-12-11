baf06ca5be59024ba5cc1b7b9adb852209bb8637
zachbeeler <zach@scalesec.com>
2023-12-11 16:18:55 -0600

    pipeline test

--- lib/database-stack.ts
+++ lib/database-stack.ts
@@ -4,23 +4,23 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as ssm from 'aws-cdk-lib/aws-ssm'
 
 export class DatabaseStack extends cdk.Stack {
-  public readonly table: dynamodb.Table;  
+  public readonly table: dynamodb.Table;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     this.table = new dynamodb.Table(this, 'Hits', {
-        partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+      partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
     })
 
     new cdk.CfnOutput(this, 'TableName', {
-        value: this.table.tableName
+      value: this.table.tableName
     })
 
     new ssm.StringParameter(
       this, 'tableName', {
-        stringValue: this.table.tableName,
-      }
+      stringValue: this.table.tableName,
+    }
     )
-
+    // Testing the pipeline
   }
 }
